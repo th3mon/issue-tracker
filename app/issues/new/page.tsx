40814a62b782efebeca9307004821873c4301ee0
th3mon <p.szelenberger@gmail.com>
2025-11-23 11:14:52 +0100

    Add Markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { useState } from "react";
+import { Button, TextField } from "@radix-ui/themes";
+import MDEditor from "@uiw/react-md-editor";
 
 type Props = {};
 
 const NewIssuePage = (props: Props) => {
+  const [value, setValue] = useState("Description");
+
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
 
-      <TextArea placeholder="Description" />
+      <MDEditor value={value} onChange={setValue} />
 
       <Button>Submit New Issue</Button>
     </div>
