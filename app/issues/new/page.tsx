fccb05ad0aa96c68dc91fa3a83aee5f66224f558
th3mon <p.szelenberger@gmail.com>
2025-11-25 00:43:09 +0100

    Move onSubmit code into its own function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -27,22 +27,20 @@ const NewIssuePage = () => {
   });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occured.");
+    }
+  });
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          setSubmitting(true);
-          await axios.post("/api/issues", data);
-
-          router.push("/issues");
-        } catch (error) {
-          setSubmitting(false);
-          setError("An unexpected error occured.");
-        }
-      })}
-    >
+    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
