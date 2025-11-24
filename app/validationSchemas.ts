4cb8d419870ce0476706a4b041e12415fe181b97
th3mon <p.szelenberger@gmail.com>
2025-11-24 23:20:28 +0100

    Move Description field error message

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,8 +2,7 @@ import { z } from "zod";
 
 export const createIssueSchema = z.object({
   title: z.string().min(1, "Title is rquired.").max(255),
-  description: z.string().min(1, "Description is rquired."),
+  description: z.string("Description is rquired.").min(1),
 });
 
 export type CreateIssueSchemaType = z.infer<typeof createIssueSchema>;
-
