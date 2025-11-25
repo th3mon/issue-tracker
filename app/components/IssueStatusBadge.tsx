075658e72e2f14f0df1e3fae5874f23f05d91369
th3mon <p.szelenberger@gmail.com>
2025-11-25 10:27:44 +0100

    Create IssueStatusBadge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@radix-ui/themes";
+import { Status } from "../generated/prisma/enums";
+
+type Color = "red" | "violet" | "green";
+type Props = {
+  status: Status;
+};
+
+const statusMap: Record<Status, { label: string; color: Color }> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "Closed", color: "green" },
+};
+
+export const IssueStatusBadge = ({ status }: Props) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
