78800f22064fb5c18b889ab352c82240cebb6fc4
th3mon <p.szelenberger@gmail.com>
2025-11-27 22:00:10 +0100

    Add Skeleton on Loading Issue Detail Page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,27 @@
+import { Box, Card, Flex, Heading } from "@radix-ui/themes";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const LoadingIssueDetailPage = () => {
-  return <div>Issue detalis are loading!</div>;
+  return (
+    <Box className="max-w-xl">
+      <SkeletonTheme
+        baseColor="hsl(272, 51.0%, 54.0%)"
+        highlightColor="hsl(272, 51.0%, 74.0%)"
+      >
+        <Heading>
+          <Skeleton />
+        </Heading>
+        <Flex gap="3" my="2">
+          <Skeleton width="5rem" />
+          <Skeleton width="8rem" />
+        </Flex>
+        <Card className="prose prose-slate prose-headings:text-slate-400 prose-strong:text-slate-500 prose-em:text-slate-400 prose-li:text-slate-400 prose-p:text-slate-300 mt-4">
+          <Skeleton count={3} />
+        </Card>
+      </SkeletonTheme>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
