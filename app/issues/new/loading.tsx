1eb1bdf983c6b63a01bff31b9e32f745365c6ff1
th3mon <p.szelenberger@gmail.com>
2025-11-27 22:17:45 +0100

    Add Skeleton on Loading New Issue Page

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,5 +1,19 @@
+import { Box } from "@radix-ui/themes";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const LoadingNewIssuePage = () => {
-  return <div>LoadingNewIssuePage</div>;
+  return (
+    <Box className="max-w-xl">
+      <SkeletonTheme
+        baseColor="hsl(272, 51.0%, 54.0%)"
+        highlightColor="hsl(272, 51.0%, 74.0%)"
+      >
+        <Skeleton />
+        <Skeleton height="20rem" />
+      </SkeletonTheme>
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
