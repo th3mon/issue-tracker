a03559c5a5206f7a670121f6c76d5d6965b58171
th3mon <p.szelenberger@gmail.com>
2025-11-28 02:32:09 +0100

    Resize IssueForm Skeleton

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -8,8 +8,8 @@ const LoadingNewIssuePage = () => {
         baseColor="hsl(272, 51.0%, 54.0%)"
         highlightColor="hsl(272, 51.0%, 74.0%)"
       >
-        <Skeleton />
-        <Skeleton height="20rem" />
+        <Skeleton height="2rem" />
+        <Skeleton height="12rem" />
       </SkeletonTheme>
     </Box>
   );
