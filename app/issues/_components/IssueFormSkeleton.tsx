08a7d8acd31044c704bcfe7118061a99bbf01ad6
th3mon <p.szelenberger@gmail.com>
2025-11-28 02:42:26 +0100

    Re-use IssueFormSkeleton

---
+++ app/issues/_components/IssueFormSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@radix-ui/themes";
+import { Skeleton, SkeletonTheme } from "@/app/components";
+
+const IssueFormSkeleton = () => {
+  return (
+    <Box className="max-w-xl">
+      <SkeletonTheme
+        baseColor="hsl(272, 51.0%, 54.0%)"
+        highlightColor="hsl(272, 51.0%, 74.0%)"
+      >
+        <Skeleton height="2rem" />
+        <Skeleton height="12rem" />
+      </SkeletonTheme>
+    </Box>
+  );
+};
+
+export default IssueFormSkeleton;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,18 +1,1 @@
-import { Box } from "@radix-ui/themes";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-
-const LoadingNewIssuePage = () => {
-  return (
-    <Box className="max-w-xl">
-      <SkeletonTheme
-        baseColor="hsl(272, 51.0%, 54.0%)"
-        highlightColor="hsl(272, 51.0%, 74.0%)"
-      >
-        <Skeleton height="2rem" />
-        <Skeleton height="12rem" />
-      </SkeletonTheme>
-    </Box>
-  );
-};
-
-export default LoadingNewIssuePage;
+export { default } from "../_components/IssueFormSkeleton";
