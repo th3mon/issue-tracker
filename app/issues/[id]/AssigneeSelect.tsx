4087d8257c63d499f65240f6ac8539b89291fcef
th3mon <p.szelenberger@gmail.com>
2025-12-01 17:39:11 +0100

    Use Users API

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,17 +1,35 @@
 "use client";
 
+import { Endpoints } from "@/app/Endpoints";
+import { User } from "@/app/generated/prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-type Props = {};
+const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
 
-const AssigneeSelect = (props: Props) => {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>(Endpoints.API_USERS);
+
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Joe Doe</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
