f33c92605e59f16c520fbeef5d7340b63042295a
th3mon <p.szelenberger@gmail.com>
2025-12-02 19:06:00 +0100

    Refactor: Organize and extract Issues list page

---
+++ app/issues/list/IssueTable.tsx
@@ -0,0 +1,91 @@
+import NextLink from "next/link";
+import { Issue, Status } from "@/app/generated/prisma/client";
+import { Table } from "@radix-ui/themes";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { IssueStatusBadge, Link } from "@/app/components";
+import { Endpoints } from "@/app/Endpoints";
+
+export type IssueQuery = {
+  status: Status;
+  orderBy: keyof Issue;
+  page: string;
+};
+
+type Props = {
+  searchParams: IssueQuery;
+  issues: Issue[];
+};
+
+const IssueTable = async ({ searchParams, issues }: Props) => {
+  return (
+    <Table.Root>
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell
+              key={column.value}
+              className={column.className}
+            >
+              <NextLink
+                href={{
+                  query: {
+                    ...searchParams,
+                    orderBy: column.value,
+                  },
+                }}
+              >
+                {column.label}
+              </NextLink>
+              {column.value === searchParams.orderBy && (
+                <ArrowUpIcon className="inline" />
+              )}
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {issues.map((issue) => (
+          <Table.Row key={issue.id}>
+            <Table.Cell>
+              <Link
+                href={Endpoints.ISSUE.replace("${issueId}", String(issue.id))}
+              >
+                {issue.title}
+              </Link>
+              <div className="md:hidden">
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              <IssueStatusBadge status={issue.status} />
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              {issue.created_at.toDateString()}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+export default IssueTable;
+
+export type Column = {
+  label: string;
+  value: keyof Issue;
+  className?: string;
+};
+
+const columns: Column[] = [
+  { label: "Issue", value: "title" },
+  { label: "Status", value: "status", className: "hidden md:table-cell" },
+  {
+    label: "Created",
+    value: "created_at",
+    className: "hidden md:table-cell",
+  },
+];
+
+export const columnNames = columns.map((column) => column.value);
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,58 +1,23 @@
 import { prisma } from "@/prisma/client";
-import { Issue } from "@/app/generated/prisma/client";
-import { Status } from "@/app/generated/prisma/enums";
-import { Box, Table } from "@radix-ui/themes";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
-import NextLink from "next/link";
-import { IssueStatusBadge, Link } from "@/app/components";
-import { Endpoints } from "@/app/Endpoints";
-import IssueActions from "./IssueActions";
+import { Issue, Status } from "@/app/generated/prisma/client";
+import { Box, Flex } from "@radix-ui/themes";
 import Pagination from "@/app/components/Pagination";
+import IssueActions from "./IssueActions";
+import IssueTable, { columnNames, IssueQuery } from "./IssueTable";
 
 type Props = {
-  searchParams: {
-    status: Status;
-    orderBy: keyof Issue;
-    page: string;
-  };
+  searchParams: IssueQuery;
 };
-
-type Column = {
-  label: string;
-  value: keyof Issue;
-  className?: string;
-};
-
-const getStatus = (status: Status): Status | undefined =>
-  Object.values(Status).includes(status) ? status : undefined;
-
-type OrderBy = Partial<Record<keyof Issue, "asc" | "desc">>;
-
-const getOrderBy = (
-  columns: Column[],
-  orderBy: keyof Issue,
-): OrderBy | undefined =>
-  columns.some((column) => column.value === orderBy)
-    ? { [orderBy]: "asc" }
-    : undefined;
 
 // TODO: Implement ASC and DESC sorting
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: Column[] = [
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    {
-      label: "Created",
-      value: "created_at",
-      className: "hidden md:table-cell",
-    },
-  ];
   const status = getStatus(searchParams.status);
   const where = { status };
-  const orderBy = getOrderBy(columns, searchParams.orderBy);
+  const orderBy = getOrderBy(columnNames, searchParams.orderBy);
   const page = Number(searchParams.page) || 1;
   // TODO: Add dropdown to select pageSize
   const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
     where,
     orderBy,
@@ -63,67 +28,28 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const itemCount = await prisma.issue.count({ where });
 
   return (
-    <Box>
+    <Flex direction="column" gap="5">
       <IssueActions />
-
-      <Table.Root mb="5">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.className}
-              >
-                <NextLink
-                  href={{
-                    query: {
-                      ...searchParams,
-                      orderBy: column.value,
-                    },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {column.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link
-                  href={Endpoints.ISSUE.replace("${issueId}", String(issue.id))}
-                >
-                  {issue.title}
-                </Link>
-                <div className="md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.created_at.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-
+      <IssueTable searchParams={searchParams} issues={issues} />
       <Pagination
         itemCount={itemCount}
         pageSize={pageSize}
         currentPage={page}
       />
-    </Box>
+    </Flex>
   );
 };
 
 export const dynamic = "force-dynamic";
 export default IssuesPage;
+
+const getStatus = (status: Status): Status | undefined =>
+  Object.values(Status).includes(status) ? status : undefined;
+
+type OrderBy = Partial<Record<keyof Issue, "asc" | "desc">>;
+
+const getOrderBy = (
+  columnNames: (keyof Issue)[],
+  orderBy: keyof Issue,
+): OrderBy | undefined =>
+  columnNames.includes(orderBy) ? { [orderBy]: "asc" } : undefined;
