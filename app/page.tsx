93ed135c9d61b63eced845c890c0a8a585bf0d7d
th3mon <p.szelenberger@gmail.com>
2025-12-02 17:36:25 +0100

    Use Pagination on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>Issue Tracker</h1>;
+import Pagination from "./components/Pagination";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <>
+      <h1>Issue Tracker</h1>
+      <Pagination
+        itemCount={666}
+        pageSize={10}
+        currentPage={Number(searchParams.page)}
+      />
+    </>
+  );
 }
