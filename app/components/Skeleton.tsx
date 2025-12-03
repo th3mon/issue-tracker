072fc4ef79f02049d8edb238671a6cdf7bc89b3a
th3mon <p.szelenberger@gmail.com>
2025-12-03 20:47:01 +0100

    Use --accent color variants

--- app/components/Skeleton.tsx
+++ app/components/Skeleton.tsx
@@ -3,10 +3,7 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const SkeletonThemeWithSetup = ({ children }: PropsWithChildren) => (
-  <SkeletonTheme
-    baseColor="hsl(272, 51.0%, 54.0%)"
-    highlightColor="hsl(272, 51.0%, 74.0%)"
-  >
+  <SkeletonTheme baseColor="var(--accent-9)" highlightColor="var(--accent-11)">
     {children}
   </SkeletonTheme>
 );
