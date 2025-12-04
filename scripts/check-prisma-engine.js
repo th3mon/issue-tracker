5a6e7cbfd27a9166fd27a6b45d95e8e5439af09e
th3mon <p.szelenberger@gmail.com>
2025-12-04 17:37:34 +0100

    Fix path to the engine

--- scripts/check-prisma-engine.js
+++ scripts/check-prisma-engine.js
@@ -4,7 +4,7 @@ const path = require("path");
 const SOURCE_ENGINE = path.resolve(
   __dirname,
   "..",
-  "app/generated/prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
+  "app/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node",
 );
 
 console.log(__dirname);
