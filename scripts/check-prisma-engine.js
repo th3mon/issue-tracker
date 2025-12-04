464d4f74289433b7a916e11b3a33a69653daddce
th3mon <p.szelenberger@gmail.com>
2025-12-04 17:21:49 +0100

    Reimplement copy script to check script

---
+++ scripts/check-prisma-engine.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+const SOURCE_ENGINE = path.resolve(
+  __dirname,
+  "..",
+  "app/generated/prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
+);
+
+console.log(__dirname);
+if (fs.existsSync(SOURCE_ENGINE)) {
+  console.log("Prisma query engine exists");
+} else {
+  console.warn("⚠️ Prisma query engine not found:", SOURCE_ENGINE);
+}
