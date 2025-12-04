fdb63d1c899ad83866e99f4c5857bdf3f404159d
th3mon <p.szelenberger@gmail.com>
2025-12-04 17:13:13 +0100

    Update prisma engine path

--- scripts/copy-prisma-engine.js
+++ scripts/copy-prisma-engine.js
@@ -4,8 +4,8 @@ const path = require("path");
 const SOURCE_ENGINE = path.resolve(
   __dirname,
   "..",
-  // "node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
-  "app/generated/prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
+  "node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
+  // "app/generated/prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
 );
 
 const TARGET_DIR = path.resolve(__dirname, "..", ".next/server/chunks");
