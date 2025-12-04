570ef660482f19952f44c4dde8c4a5b2d018f40e
th3mon <p.szelenberger@gmail.com>
2025-12-04 13:23:07 +0100

    Remove dev prisma migrations

--- prisma/migrations/20251201174752_add_assigned_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Issue` ADD COLUMN `assigned_to_user_id` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assigned_to_user_id_fkey` FOREIGN KEY (`assigned_to_user_id`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20251204121329_add_pk_to_verification_token/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `VerificationToken` ADD PRIMARY KEY (`identifier`, `token`);
-
--- DropIndex
-DROP INDEX `VerificationToken_identifier_token_key` ON `VerificationToken`;
